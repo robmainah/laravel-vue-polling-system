f75712757a7e4cf6750bfb67a3c27d3df1ecd4e6
robmainah <robmainah@gmail.com>
2024-04-18 13:42:18 +0300

    create migrations

--- database/migrations/2024_04_18_083014_create_choices_table.php
+++ database/migrations/2024_04_18_083014_create_choices_table.php
@@ -1,11 +1,11 @@
 <?php
 
+use App\Models\Question;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class () extends Migration {
     /**
      * Run the migrations.
      */
@@ -13,6 +13,8 @@ return new class extends Migration
     {
         Schema::create('choices', function (Blueprint $table) {
             $table->id();
+            $table->foreignIdFor(Question::class)->constrained()->cascadeOnDelete();
+            $table->text('content');
             $table->timestamps();
         });
     }
--- database/migrations/2024_04_18_083023_create_votes_table.php
+++ database/migrations/2024_04_18_083023_create_votes_table.php
@@ -1,11 +1,13 @@
 <?php
 
+use App\Models\Choice;
+use App\Models\Question;
+use App\Models\User;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class () extends Migration {
     /**
      * Run the migrations.
      */
@@ -13,6 +15,9 @@ return new class extends Migration
     {
         Schema::create('votes', function (Blueprint $table) {
             $table->id();
+            $table->foreignIdFor(User::class)->constrained()->cascadeOnDelete();
+            $table->foreignIdFor(Question::class)->constrained()->cascadeOnDelete();
+            $table->foreignIdFor(Choice::class)->constrained()->cascadeOnDelete();
             $table->timestamps();
         });
     }
