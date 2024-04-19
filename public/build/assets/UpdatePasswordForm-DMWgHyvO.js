4bed7e5a8afdc9aa0572d729ec7d16faf2588e2d
robmainah <robmainah@gmail.com>
2024-04-19 20:47:19 +0300

    upload build folder. Shouldn't happen for production but this is just for devs only and testing

---
+++ public/build/assets/UpdatePasswordForm-DMWgHyvO.js
@@ -0,0 +1,1 @@
+import{_ as o}from"./UpdatePasswordForm.vue_vue_type_script_setup_true_lang-DK1c5M3_.js";import"./app-CTAVDbhm.js";import"./TextInput.vue_vue_type_script_setup_true_lang-DzHeBdDn.js";import"./PrimaryButton-B_ksmL4X.js";import"./_plugin-vue_export-helper-DlAUqK2U.js";export{o as default};
